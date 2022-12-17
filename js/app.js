6065883749fdb5053a236eae61d724d4e3132c6f
branhickman <branhickman@gmail.com>
2022-12-16 18:10:16 -0700

    add animations for feed/nap/set name functions

--- js/app.js
+++ js/app.js
@@ -1,62 +1,47 @@
 /*-------------------------------- Constants --------------------------------*/
 
 // not sure if this should be a constantr
-let petName
-
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let petName
 let foodSupply
 let healthMeter
 let petWeight
 let isSleeping
 let isPlaying
-
-let dancingAnimation = `<img id="shar-pei" src="./assets/sleeping-animation.gif" alt="shar pei dog">`
-
+let isEating
 /*------------------------ Cached Element References ------------------------*/
-
+// cached buttons
 const feedBtn = document.querySelector("#feed-btn")
 const playBtn = document.querySelector("#play-btn")
 const napBtn = document.querySelector("#nap-btn")
 const resetBtn = document.querySelector("#reset-btn")
-// just edited this, was looking at the wrong thing
-const nameEl = document.querySelector("#name")
-const statsPanel = document.querySelector(".stats-panel")
-const nameInput = document.querySelector("#name-input")
 const nameSubmitBtn = document.querySelector("#submit-name")
+// user input
+const nameInput = document.querySelector("#name-input")
+// used to control animation renders
+const animationImg = document.querySelector("#animation")
+// primary ui elements
+const nameEl = document.querySelector("#name")
+const statsPanelEl = document.querySelector(".stats-panel")
 const foodSupplyEl = document.querySelector("#food-supply")
 const alertsPanelEl = document.querySelector("#alerts-panel")
 const healthMeterEl = document.querySelector("#health-meter")
 const petWeightEl = document.querySelector("#pet-weight")
-
-const statsHeader = document.querySelector("#stats-header")
 const mainDisplayEl = document.querySelector(".main-display")
+// cached animations
+const leftFacingAnimation = "../assets/left-facing.png"
+const happyAnimation = "../assets/happy-animation.gif"
+const eatingAnimation = "../assets/eating-animation.gif"
+const sleepingAnimation = "../assets/sleeping-animation.gif"
 /*----------------------------- Event Listeners -----------------------------*/
-
 feedBtn.addEventListener('click', feedPet)
 playBtn.addEventListener('click', playPet)
 napBtn.addEventListener('click', takeNap)
 nameSubmitBtn.addEventListener('click', submitName)
 resetBtn.addEventListener('click', reset)
-
-// quick tests to ensure buttons work
-// function feedPet(){
-//   console.log("button works")
-// }
-function playPet(){
-  console.log("button works")
-}
-function takeNap(){
-  console.log("button works")
-}
-// defined so that I can refrain from receiving a console error
-function reset(){}
-
 /*-------------------------------- Functions --------------------------------*/
 init()
-
-// create a function to initialize the game
 
 function init(){
   healthMeter = 1
@@ -66,16 +51,13 @@ function init(){
   setInitialFoodQuantity()
   updateHealthMeter()
   updatePetWeight()
+  // need to add initial animation with id of #animation
 }
-
-//* create a function that sets an inital quantiy of food items, store this quantity in a variable, update the ui
 
 function setInitialFoodQuantity(){
   foodSupply = 1
   foodSupplyEl.textContent = ("Food Supply: " + foodSupply)
 }
-
-// create a function to render game state
 
 function render(){
   updateHealthMeter()
@@ -95,106 +77,107 @@ function updatePetWeight(){
   petWeightEl.textContent = ("Pet Weight: " + petWeight)
 }
 
-//* create a function that prompts the user to input a name for their pet, store the name in a variable to display in UI, trigger confetti or another celebratory animation
+function updateAnimation(animationName){
+  document.querySelector(".main-display").removeChild(document.querySelector("#animation"))
+  let updatedAnimation = document.createElement("img")
+  updatedAnimation.setAttribute("src", animationName)
+  updatedAnimation.setAttribute('width', "40%")
+  updatedAnimation.setAttribute("id","animation")
+  mainDisplayEl.appendChild(updatedAnimation)
+}
+
+function reset(){
+  location.reload();
+  return false;
+}
 
 function submitName(){
-  console.log("button works")
   petName = nameInput.value
-  console.log(petName)
   nameEl.textContent = ("Name: "+ petName)
-  statsPanel.removeChild(nameInput)
-  statsPanel.removeChild(nameSubmitBtn)
+  statsPanelEl.removeChild(nameInput)
+  statsPanelEl.removeChild(nameSubmitBtn)
   confetti.start(1500)
+  updateAnimation(happyAnimation)
+  setTimeout(() => {
+    updateAnimation(leftFacingAnimation)
+  }, 3000)
   alertsPanelEl.textContent = ("You Named your Pet " + petName)
   setTimeout(() => {
     alertsPanelEl.textContent = ""
   }, 5000)
-  // add timer to remove alert
-  // need to add happy animation
 }
-
-// experiment area for making gifs appear
-function addElement(){
-  let newGif = document.createElement("img")
-  newGif.setAttribute("src", "./assets/sleeping-animation.gif")
-  newGif.setAttribute('width', "80%")
-  mainDisplayEl.appendChild(newGif)
-}
-
-// let newGif = petAnimationDisplay.createElement("img")
-// newGif.setAttribute("src","./assets/sleeping-animation.gif")
-
-//* create a function that will allow a user to feed their pet and increment the pet's health meter, increment the pets weight, and decrement the user's food supply, update health meter/pet weight/food supply in UI, trigger a happy pet animation
 
 function feedPet(){
-  if(foodSupply >=1){
-    foodSupply--
-    healthMeter++
-    petWeight++
-    render()
-    alertsPanelEl.textContent = ("You Fed " + petName)
-    setTimeout(() => {
-      alertsPanelEl.textContent = ""
-    }, 5000)
-    // add happy pet animation 
-  } else {
-    alertsPanelEl.textContent = " not enough food"
+  if(isEating === true){
+    alertsPanelEl.textContent = (petName + " is Already Eating")
+  }else {
+    if(foodSupply >=1){
+      foodSupply--
+      healthMeter++
+      petWeight++
+      isEating = true
+      alertsPanelEl.textContent = ("You Fed " + petName)
+      render()
+      updateAnimation(eatingAnimation)
+      setTimeout(() => {
+        updateAnimation(leftFacingAnimation)
+      }, 5600)
+      setTimeout(() => {
+        alertsPanelEl.textContent = ""
+      }, 5600)
+    } else {
+      // if time, put a sad dog animation
+      alertsPanelEl.textContent = " not enough food"
+    }
   }
 }
 
-// create a function that will decrement the pet's health meter if neglected (left for a certain period of time without activity), update health meter in UI, trigger a sad pet animation
-
-function neglectedPet(){
-
-}
-
-
-//* create a function that will allow your pet to take a nap, incrementing health meter, displaying in UI and triggering an animation // level up by having a sleepy meter to indicate when your pet needs sleep
-
+// level up by having a sleepy meter to indicate when your pet needs sleep
 function takeNap(){
   // maybe setup a timer that says you can't take a nap but x times every x minutes
-  healthMeter++
-  foodSupply++
-  isNapping = true
-  render()
-  alertsPanelEl.textContent = (petName + " is Napping")
-  setTimeout(() => {
-    isNapping = false
-    alertsPanelEl.textContent = (petName +" is Done Napping")
-  }, 5000)
-  setTimeout(() => {
-    alertsPanelEl.textContent = ""
-  }, 10000)
-  // add animation 
   // add sleepy meter
+  if(isNapping === true){
+    alertsPanelEl.textContent = (petName + " is Already Napping")
+  } else {
+    healthMeter++
+    foodSupply++
+    isNapping = true
+    render()
+    updateAnimation(sleepingAnimation)
+    alertsPanelEl.textContent = (petName + " is Napping")
+    setTimeout(() => {
+      updateAnimation(leftFacingAnimation)
+      alertsPanelEl.textContent = (petName +" is Done Napping")
+      isNapping = false
+    }, 6800)
+    setTimeout(() => {
+      alertsPanelEl.textContent = ""
+    }, 8800)
+  }
 }
 
 //* create a function that will allow user to play with their pet, incrementing health meter, triggering a play animation ('shar-ing' aka zoomies)
 
 function playPet(){
-  // maybe setup a timer that says you can't take a nap but x times every x minutes
-  foodSupply++
-  healthMeter++
-  isPlaying = true
-  alertsPanelEl.textContent = (petName + " is Playing")
-  render()
-  setTimeout(() => {
-    isPlaying = false
-    alertsPanelEl.textContent = (petName + " is Done Playing")
-  }, 5000)
-  setTimeout(() => {
-    alertsPanelEl.textContent = ""
-  }, 10000)
-  // add play animation
+  if(isPlaying === true){
+    alertsPanelEl.textContent = (petName + " is Already Playing")
+  } else {
+    foodSupply++
+    healthMeter++
+    isPlaying = true
+    alertsPanelEl.textContent = (petName + " is Playing")
+    render()
+    setTimeout(() => {
+      isPlaying = false
+      alertsPanelEl.textContent = (petName + " is Done Playing")
+    }, 5000)
+    setTimeout(() => {
+      alertsPanelEl.textContent = ""
+    }, 10000)
+    // add play animation
+  }
 }
 
-// create a function to reset pet stats so that a user can start over, include a "are you sure?" prompt
-
-// add this last
-function reset(){
-  location.reload();
-  return false;
-}
 
 // create a function that increases pet's age every x interval
 
@@ -205,3 +188,17 @@ function reset(){
 // LEVEL UP IDEA: Create a function that allows a pet to go to the bathroom, must include functionality that raises pet's bladder level and displays onscreen, health meter decrements by x amount during a specific time frequency if bladder is full and the pet hasn't relieved themselves, trigger animation for using restroom or animation of 'having an accident' if the bladder is left full for too long
 
 // LEVEL UP IDEA: Create an intial crawl animation of text that tells the store of sharmagotchi being adopted, give him a name, and make sure theyre the happiest pup in the world
+
+// use age to have the width of the dog grow over time
+
+// setup audio snippets for actions in the game
+
+// setup other audio files to not play if isPlaying is true
+  // for mute, set audio level to 0 with audiofile.volume = .05
+  //audioFile.pause() works
+
+  // LEVEL UP create a function that will decrement the pet's health meter if neglected (left for a certain period of time without activity), update health meter in UI, trigger a sad pet animation
+
+function neglectedPet(){
+
+}
